450ca6cfe86fbd12dba69f5f7d1a9b5b616cbeda
Naufal Akbar <naufalakbar378@gmail.com>
2024-10-10 08:40:14 +0700

    upd: create new page

---
+++ src/lib/components/Containers/AddPatient/AddPatient.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { FileUser } from 'lucide-svelte';
+
+	import Heading from '$lib/components/Common/Heading.svelte';
+</script>
+
+<section class="container">
+	<!-- TOP SECTION -->
+	<div class="flex flex-col justify-between w-full lg:mb-5 md:items-center md:flex-row">
+		<Heading className="mb-5 lg:mb-0">
+			<FileUser class="mt-1" size={30} slot="icon" />
+			<h1 class="text-3xl font-semibold tracking-tight" slot="title">Tambah Pasien</h1>
+		</Heading>
+	</div>
+</section>
---
+++ src/routes/add/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import AddPatient from '$lib/components/Containers/AddPatient/AddPatient.svelte';
+</script>
+
+<svelte:head>
+	<title>Tambah Pasien</title>
+	<meta name="description" content="Tambah Pasien PT Sabbi - PT Maxchat Test FE" />
+</svelte:head>
+
+<AddPatient />
